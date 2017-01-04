bbb962adf099879b50c74edf73ae955f0fd3b8c2
12006684 <12006684@e107m05.istic.univ-rennes1.fr>
2017-01-04 17:56:56 +0100

    new

---
+++ src/main/java/fr/istic/sir/JXBusyLabelTest/App.java
@@ -0,0 +1,23 @@
+package fr.istic.sir.JXBusyLabelTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	
+	public void foo() {
+		int a = 1+1;
+		System.out.println(a);
+	}
+	
+
+	
+    public void maino()
+    {
+    	int a = 1+1;
+        System.out.println( "Hello World!" );
+    }
+}
